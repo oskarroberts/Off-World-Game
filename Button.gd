ff6a36503644b0f6a41a7c6ba0c010aa5608c53d
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-17 14:15:06 +1200

    CreatedDay1

--- Button.gd
+++ Button.gd
@@ -12,4 +12,5 @@ func _process(delta):
 
 
 func _on_pressed():
-	Dialogic.start("timeline_Test")
+	get_tree().change_scene_to_file("res://Day1.tscn")
+	
--- control.gd
+++ control.gd
@@ -4,9 +4,12 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Dialogic.signal_event.connect(_on_dialogic_signal)
-	Dialogic.start("timetest")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_dialogic_signal(argument: String):
+	if argument == "ChangeSceneMoney":
+		print("awesomesauce")
