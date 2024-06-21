cc4b1c15822c3b678122632904f73ec481496927
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-21 15:13:50 +1200

    Dialogic Signals Done, Working on Displaying variables

--- Label.gd
+++ Label.gd
@@ -1,15 +1,18 @@
-extends Node2D
+extends Label
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Dialogic.signal_event.connect(_on_dialogic_signal)
 
+#moneyvar 
+var money = 0
 
+#Money signal receive
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
-
 func _on_dialogic_signal(argument: String):
-	if argument == "ChangeSceneMoney":
-		print("awesomesauce")
+	if argument == "Moneyplus1": 
+		money += 1
+		print("()")
