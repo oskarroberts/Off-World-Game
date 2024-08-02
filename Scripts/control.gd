1bfa9822e9eda1429ac50d2d29c6e03d10c57627
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-08-02 12:40:23 +1200

    I FINALLY GOT UI INFRONT OF BG WOOHOO

--- Scripts/control.gd
+++ Scripts/control.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+var style: DialogicStyle = load("res://Styles/Style_Test.tres")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	var style: DialogicStyle = load("res://Styles/Style_Test.tres")
+	style.prepare
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
