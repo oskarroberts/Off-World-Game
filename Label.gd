d49d189487d91e10c26de6945badf242f4b8dd77
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-08-16 12:47:55 +1200

    16/08/24

--- Label.gd
+++ Label.gd
@@ -4,7 +4,6 @@ extends Label
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Dialogic.signal_event.connect(_on_dialogic_signal)
-
 #Money signal receive
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
