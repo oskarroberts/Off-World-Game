6c51a8c539b2e51d68369ebc9d2666df50653337
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-09-23 10:52:20 +1200

    23/09/24

--- Scripts/Ui.gd
+++ Scripts/Ui.gd
@@ -4,6 +4,7 @@ extends CanvasLayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.visible = false
+	Dialogic.signal_event.connect(_on_dialogic_signal)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,3 +14,7 @@ func _process(delta):
 
 func _on_button_pressed():
 	self.visible = true
+
+func _on_dialogic_signal(argument: String):
+	if argument == "HideUI": 
+		self.visible = false
